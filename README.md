21cf315426c57893e93aded491ee8e7ca46396a1
Arjun Parihar <76870260+TonyStark2021@users.noreply.github.com>
2021-03-19 16:12:55 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# MatterJSBoilerPlate
-MatterJSBoilerPlate
+Geologist Project
--- sketch.js
+++ sketch.js
@@ -1,79 +1,54 @@
 const Engine = Matter.Engine;
-const World = Matter.World;
+const World= Matter.World;
 const Bodies = Matter.Bodies;
 
-// Declaring the engine variable.
+var engine, world;
+var stone, iron,rubber,hammer;
+var sand1, sand2, sand3, sand4,sand5,sand6;
 
-var engine;
-var hammer,plane,stone,rubber;
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
 
-function preload()
-{
-	
+    plane = new Plane(600,height,1200,20)
+    
+    stone = new Stone(700,320,100,100);
+    iron = new Iron(300,350);
+    rubber=new Rubber(900,450,70);
+    hammer = new Hammer(10,100);
+
+    sand1 = new Sand(505,450,10);
+    sand2 = new Sand(510,450,10);
+    sand3 = new Sand(515,450,10);
+    sand4 = new Sand(520,450,10);
+    sand5 = new Sand(525,450,10);
+    sand6 = new Sand(515,445,10);
+    sand7 = new Sand(520,445,10);
+    sand8 = new Sand(525,445,10);
+    sand9 = new Sand(530,445,10);
+    sand10 = new Sand(535,445,10);
 }
 
-function setup() {
-	createCanvas(1200,400);
-
-    //Creating an engine for the application to make physics simulations.
-
-	engine = Engine.create();
-	world = engine.world;
-
-	//Creating the bodies.
-
-    plane = new Plane(600,400,1250,20);
-
-    hammer = new Hammer(800,200,20,20);
-
-	stone = new Stone(600,200,90,90);
-
-	rubber = new Rubber(400,200,20,10);
-
-	iron = new Iron(200,200,75,50);
-
-	sand1 = new Sand(500,500);
-
-	sand2 = new Sand(500,500);
-
-	sand3 = new Sand(500,500);
-
-	sand4 = new Sand(500,500);
-
-	sand5 = new Sand(500,500);
-
-	sand6 = new Sand(500,500);
-
-	sand7 = new Sand(500,500);
-
-	sand8 = new Sand(500,500);
-
-	sand9 = new Sand(500,500);
-
-	sand10 = new Sand(500,500);
-
-}
-
-//Displaying the objects.
-
-function draw() {
-	background("lightblue");
-
+function draw(){
+    background("rgb(160,219,221)");
     Engine.update(engine);
 
-	hammer.display();
-	plane.display();
-	stone.display();
-	rubber.display();
-	iron.display();
-	sand1.display();
-	sand2.display();
-	sand3.display();
-	sand4.display();
-	sand5.display();
-	sand6.display();
-	sand7.display();
-	sand8.display();
-	sand9.display();
-	sand10.display();
+    stone.display();
+    plane.display();
+    iron.display();
+    rubber.display();
+
+    hammer.display();
+
+    sand1.display();
+    sand2.display();
+    sand3.display();
+    sand4.display();
+    sand5.display();
+    sand6.display();
+    sand7.display();
+    sand8.display();
+    sand9.display();
+    sand10.display();
 }
